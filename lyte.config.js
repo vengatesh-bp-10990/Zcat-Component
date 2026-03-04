ba1eee1d8893d5f3298463c84bece540a9578504
VengateshB12 <vengi9360@gmail.com>
2026-03-04 15:27:32 +0530

    Fix: Revert invalid lyte.config hook and correct catalyst.json source to dist/

--- lyte.config.js
+++ lyte.config.js
@@ -1,20 +1,14 @@
-let path = require("path");
-let fs = require("fs");
-
+// let path = require("path");
 module.exports = function(options) {
     return {
-        // Hook to copy web.config to dist after build
-        onBuildEnd: function(buildResult) {
-            try {
-                const srcWebConfig = path.join(options.root, "web.config");
-                const distWebConfig = path.join(options.root, "dist", "web.config");
-                if (fs.existsSync(srcWebConfig)) {
-                    fs.copyFileSync(srcWebConfig, distWebConfig);
-                    console.log("✓ Copied web.config to dist/");
-                }
-            } catch (e) {
-                console.warn("Could not copy web.config:", e.message);
-            }
-        }
+        // initialFileToLoad : path.join(options.root,"app-init.js"),
+        
+        // entry : {
+            // key : Array of values
+        // }
+        // Eg : entry : {
+        //     "components/todo.js" : ["components/javascript/todo-item.js","components/javascript/todo-modal.js"]
+        // }
+         
     }
 }
